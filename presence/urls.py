0145ff8abe53ed9e9903094d21d586667cf55505
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-08-13 18:42:05 +0700

    add take presence and rename from Redirect to Landing

--- presence/urls.py
+++ presence/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('join-class', join_class, name='join-class'),
     path('data-completions', complete_data, name='complete-data'),
     path('class-list', ClassList.as_view(), name='class-list'),
-    path('', LandingView.as_view(), name='redir'),
+    path('presence', Presence.as_view(), name='take-presence'),
+    path('', LandingView.as_view(), name='landing'),
 ]
