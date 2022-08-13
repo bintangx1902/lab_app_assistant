2d1d87dc83ae185f6976ecfb2c284aaa92866318
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-08-13 18:39:48 +0700

    add class list and generate qr in urlpath

--- assistant/urls.py
+++ assistant/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('file-view', SeeAllFiles.as_view(), name='files'),
     path('create-class', create_class, name='create-class'),
     path('backup-option', backup, name='backup'),
+    path('my-class', MyClassList.as_view(), name='my-class-list'),
+    path('my-class/<slug:link>/generate-qr', GenerateQRCodeView.as_view(), name='generate-qr'),
     path('my-class/<slug:link>', MyClass.as_view(), name='my-class'),
 ]
