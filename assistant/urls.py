9ce6b3a321adbb962928495071c6a50c64d20208
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-08-15 00:03:00 +0700

    remove file-view and replace with per class file view

--- assistant/urls.py
+++ assistant/urls.py
@@ -5,10 +5,10 @@ app_name = 'assist'
 
 urlpatterns = [
     path('', AssistantLanding.as_view(), name='landing'),
-    path('file-view', SeeAllFiles.as_view(), name='files'),
     path('create-class', create_class, name='create-class'),
     path('backup-option', backup, name='backup'),
     path('my-class', MyClassList.as_view(), name='my-class-list'),
     path('my-class/<slug:link>/generate-qr', GenerateQRCodeView.as_view(), name='generate-qr'),
+    path('my-class/<slug:link>/file', SeeAllFiles.as_view(), name='file-class'),
     path('my-class/<slug:link>', MyClass.as_view(), name='my-class'),
 ]
