bbe899dea2579c4fe503231cb130b9187cbb36c9
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-08-17 17:31:41 +0700

    add join class to url path

--- assistant/urls.py
+++ assistant/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', AssistantLanding.as_view(), name='landing'),
     path('create-class', create_class, name='create-class'),
     path('backup-option', backup, name='backup'),
+    path('join-class', JoinAssistantClas.as_view(), name='join-class'),
     path('my-class', MyClassList.as_view(), name='my-class-list'),
     path('my-class/<slug:link>/generate-qr', GenerateQRCodeView.as_view(), name='generate-qr'),
     path('my-class/<slug:link>/file/delete/<int:item_pk>', delete_file, name='delete-file'),
