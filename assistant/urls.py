e12ae07a0eaad10fb85555906bb3533495af7529
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-08-29 18:33:30 +0700

    the function's name in views.py was changed

--- assistant/urls.py
+++ assistant/urls.py
@@ -5,12 +5,11 @@ app_name = 'assist'
 
 urlpatterns = [
     path('', AssistantLanding.as_view(), name='landing'),
-    path('create-class', create_class, name='create-class'),
-    path('backup-option', backup, name='backup'),
+    path('create-class', CreateClass.as_view(), name='create-class'),
     path('join-class', JoinAssistantClas.as_view(), name='join-class'),
     path('my-class', MyClassList.as_view(), name='my-class-list'),
     path('my-class/<slug:link>/generate-qr', GenerateQRCodeView.as_view(), name='generate-qr'),
-    path('my-class/<slug:link>/file/delete/<int:item_pk>', delete_file, name='delete-file'),
+    path('my-class/<slug:link>/file/delete/<int:item_pk>', DeleteFile.as_view(), name='delete-file'),
     path('my-class/<slug:link>/file', SeeAllFiles.as_view(), name='file-class'),
     path('my-class/<slug:link>', MyClass.as_view(), name='my-class'),
 ]
