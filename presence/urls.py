e1b6e511f52c903145bb9d8ead41d1bf4a5ea4eb
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-08-29 18:25:24 +0700

    the name for view was changed in views.py

--- presence/urls.py
+++ presence/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 app_name = 'presence'
 
 urlpatterns = [
-    path('join-class', join_class, name='join-class'),
-    path('data-completions', complete_data, name='complete-data'),
+    path('join-class', JoinClass.as_view(), name='join-class'),
+    path('data-completions', DataComplement.as_view(), name='complete-data'),
     path('class-list', ClassList.as_view(), name='class-list'),
     path('presence', Presence.as_view(), name='take-presence'),
     path('class/<slug:link>/upload-file', UploadFileByClass.as_view(), name='upload-file'),
