d3bea41a53be1d330a46e78dc9bee82b8d67bdfb
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-09-02 22:43:54 +0700

    renaming from class-lsit to class and add recaps

--- presence/urls.py
+++ presence/urls.py
@@ -6,9 +6,10 @@ app_name = 'presence'
 urlpatterns = [
     path('join-class', JoinClass.as_view(), name='join-class'),
     path('data-completions', DataComplement.as_view(), name='complete-data'),
-    path('class-list', ClassList.as_view(), name='class-list'),
     path('presence', Presence.as_view(), name='take-presence'),
+    path('class', ClassList.as_view(), name='class-list'),
     path('class/<slug:link>/upload-file', UploadFileByClass.as_view(), name='upload-file'),
     path('class/<slug:link>/file-list', FileListClass.as_view(), name='file-list-class'),
+    path('class/<slug:link>/recaps', MyPresenceRecaps.as_view(), name='my-presence-recap'),
     path('', LandingView.as_view(), name='landing'),
 ]
