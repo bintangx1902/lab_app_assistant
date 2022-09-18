ad4225c6af2854579999de3a61f6db5a36264115
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-09-18 14:41:37 +0700

    add login, logout, authenticated user in url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import TakePresenceEndPoint
+
+from .views import *
 
 app_name = 'api'
 
-
 urlpatterns = [
-    path('take-presence', TakePresenceEndPoint.as_view(), name='take-presence')
+    path('take-presence', TakePresenceEndPoint.as_view(), name='take-presence'),
+    path('login', LoginEndPoint.as_view(), name='login'),
+    path('logout', LogoutEndPoint.as_view(), name='logout'),
+    path('auth-user', AuthenticatedUser.as_view(), name='auth-user'),
 ]
