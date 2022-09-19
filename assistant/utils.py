e701300ac0ad5db4d080f0ce8d9d8a36c7ebb13e
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-09-19 23:23:33 +0700

    create class name and link

--- assistant/utils.py
+++ assistant/utils.py
@@ -1,4 +1,9 @@
-import string, random
+import random
+import re
+import string
+
+class_list = [f"Kelas {char}" for char in string.ascii_uppercase]
+course_list = ['PBO', 'Struktur Data', 'Dasar Pemrograman', 'Pemrograman Deklaratif']
 
 
 def slug_generator(n):
@@ -14,3 +19,16 @@ def check_slug(link: str, link_list: list, n):
             link = slug_generator(n)
         else:
             return link
+
+
+def set_class_name(start, end, class_, gen, course):
+    punc = r'[' + string.punctuation + ']'
+    name = f"{gen} {class_} {course} Jam"
+    name = re.sub(punc, '', name)
+    class_name = f"{name} {start}-{end}"
+
+    start = re.sub(punc, '', start)
+    end = re.sub(punc, '', end)
+    class_link = f"{name} {start}-{end}".replace(' ', '-')
+
+    return class_name, class_link
