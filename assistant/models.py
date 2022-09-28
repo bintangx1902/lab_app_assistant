d9c1938bdc14db434fc4a2905c55eee8318aa8dd
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-09-28 11:44:01 +0700

    models for generate token

--- assistant/models.py
+++ assistant/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+class TokenToResetPassword(models.Model):
+    token = models.SlugField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    valid_until = models.DateTimeField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, related_name='user_pass', related_query_name='user_pass', blank=True, null=True)
+
+    def __str__(self):
+        now = timezone.now()
+        return f"{self.token} is {'expired' if now > self.valid_until else 'available'}"
+
+    def val_stamp(self):
+        return self.valid_until.strftime('%a %H:%M  %d/%m/%y')
 
