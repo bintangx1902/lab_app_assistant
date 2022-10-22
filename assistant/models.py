3fb04a17502f3fda3f6b619270babd5e40f0dfe9
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-10-22 17:11:13 +0700

    fixing score mdoels

--- assistant/models.py
+++ assistant/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from presence.models import ClassName
 
 
 class TokenToResetPassword(models.Model):
@@ -21,8 +22,10 @@ class TokenToResetPassword(models.Model):
 class StudentScore(models.Model):
     name = models.CharField(max_length=255)
     score = models.FloatField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='score', related_query_name='score')
     classification = models.CharField(max_length=255)
+    class_name = models.ForeignKey(ClassName, on_delete=models.CASCADE, related_name='user_score',
+                                   related_query_name='user_score')
 
     def __str__(self):
         return f"{self.user.username} - {self.name} : {self.score}"
