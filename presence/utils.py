e5d305976862348b03d1a8413b031c1c112387bd
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-11-02 23:35:55 +0700

    create a function to count average score

--- presence/utils.py
+++ presence/utils.py
@@ -2,3 +2,7 @@ def check_nim(nim_list: list, target: str):
     if target in nim_list:
         return f"{target} sudah terdaftar di sistem", True
     return "", False
+
+
+def total_average(models, Avg):
+    return models.aggregate(Avg('score'))
