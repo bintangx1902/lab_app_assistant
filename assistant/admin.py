2265e2ce7813d71bcf5304e6b6f667ffdca26d18
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-03-20 13:13:33 +0700

    registering request password models to admin

--- assistant/admin.py
+++ assistant/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TokenToResetPassword, StudentScore
+from .models import *
 
 admin.site.register(TokenToResetPassword)
 admin.site.register(StudentScore)
+admin.site.register(ResetPasswordRequest)
