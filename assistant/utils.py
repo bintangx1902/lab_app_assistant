b02a6bb01c00f6df4e373ed8ad699e041bfa30f2
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-03-20 13:14:29 +0700

    add new course name

--- assistant/utils.py
+++ assistant/utils.py
@@ -3,7 +3,7 @@ import re
 import string
 
 class_list = [f"Kelas {char}" for char in string.ascii_uppercase]
-course_list = ['PBO', 'Struktur Data', 'Dasar Pemrograman', 'Pemrograman Deklaratif', 'Pemrograman Mobile Lanjut', 'Pemrograman Web Lanjut']
+course_list = ['PBO', 'Struktur Data', 'Dasar Pemrograman', 'Pemrograman Deklaratif', 'Pemrograman Mobile Lanjut', 'Pemrograman Web Lanjut', 'Pengantar Basis Data']
 conditions = ['Izin', 'Sakit']
 
 
