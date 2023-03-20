7441b90f51ac08c3c3226f1b0fcb9366136a6b5d
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-03-20 13:14:51 +0700

    add email backend

--- labs/settings.py
+++ labs/settings.py
@@ -128,13 +128,17 @@ AUTHENTICATION_BACKENDS = [
 
 ROOT_URLCONF = 'labs.urls'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.office365.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = config('LAB_EMAIL')
-# EMAIL_HOST_PASSWORD = config('LAB_PASSWORD')
-# DEFAULT_FROM_EMAIL = config('LAB_EMAIL')
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = config('LAB_EMAIL')
+    EMAIL_HOST_PASSWORD = config('LAB_PASSWORD')
+    DEFAULT_FROM_EMAIL = config('LAB_EMAIL')
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 TEMPLATES = [
     {
